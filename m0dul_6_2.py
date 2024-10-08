32261c9b6735b6e3b777fecac7804edfc63e7c77
user <piterts65@gmail.com>
2024-10-08 12:45:02 +0500

    ?++

--- m0dul_6_2.py
+++ m0dul_6_2.py
@@ -9,7 +9,7 @@ class Vehicle:
 
 
     def get_model(self, __model):
-        print(f'Модель: {__model}')
+        return f'Модель: {__model}'
 
     def get_horsepower(self, __engine_power):
         return f'Мощность двигателя: {__engine_power}'
@@ -24,17 +24,19 @@ class Vehicle:
         print(self.get_color(self.__color))
         print(f'Владелец: {self.owner}')
 
-    def set_color(self, new_color: str):
-        for __color in Vehicle.__COLOR_VARIANTS:
-            if new_color.upper() == __color:
-                self.__color = new_color
-            return self.__color
-        else:
+    def set_color(self, new_color='white'):
+        color_variants = list(map(str.upper, self.__COLOR_VARIANTS))
+        if new_color.upper() not in color_variants:
             print(f"Нельзя сменить цвет на {new_color}")
+        else:
+            self.__color = new_color
+            return self.__color
 
 
 class Sedan(Vehicle):
     __PASSENGERS_LIMIT = 5
+
+
 
 
 # Текущие цветa
@@ -47,6 +49,7 @@ vehicle1.print_info()
 # Меняем свойства (в т.ч. вызывая методы)
 vehicle1.set_color('Pink')
 vehicle1.set_color('BLACK')
+vehicle1.owner = 'Vasyok'
 
 # Проверяем что поменялось
 vehicle1.print_info()
